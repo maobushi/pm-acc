7726f60f0f4c459a21cdc35816dc5987ff8f445d
Cyanea <g3tsuzumi@gmail.com>
2024-11-16 12:59:42 +0700

    add contract

---
+++ contract/contracts/OptionMarket.sol
@@ -0,0 +1,116 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./interface/IPMT.sol";
+import "hardhat/console.sol";
+
+contract OptionMarket {
+	uint256 constant SCALE = 10**18;
+
+	function onERC1155Received(
+        address operator,
+        address from,
+        uint256 id,
+        uint256 value,
+        bytes calldata data
+    ) external returns (bytes4) {
+        // Handle the token transfer
+        return this.onERC1155Received.selector;
+    }
+
+	// CPMMを使用して動的価格を算出
+	function calculateOptionChange(
+		address target, 
+		uint256 opt, 
+		uint256 dx, 
+		bool isBuy
+	) external view returns (uint256 dy, uint256 newCollateralBalance, uint256 newOptionBalance) {
+		IPMT t = IPMT(target);
+		require(opt < t.getOptions().length, "Invalid option selected");
+		require(dx > 0, "Deposit amount must be greater than zero");
+
+		uint256 collateralBalanceBefore = t.getBalanceOfCollateralPool();
+		uint256 optionBalanceBefore = t.getBalanceOfOptionPool(opt);
+		
+		uint256 k = collateralBalanceBefore * optionBalanceBefore;
+		uint256 collateralAfter;
+		uint256 optionBalanceAfter;
+
+		if (isBuy) {
+			collateralAfter = collateralBalanceBefore + dx;
+			optionBalanceAfter = k / collateralAfter;
+			dy = optionBalanceBefore - optionBalanceAfter;
+		} else {
+			require(dx <= optionBalanceBefore, "Insufficient options in pool");
+
+			optionBalanceAfter = optionBalanceBefore + dx;
+			
+			uint256 temp = k / optionBalanceAfter;
+			uint256 remainder = k % optionBalanceAfter;
+			uint256 roundValue = (2 * remainder >= optionBalanceAfter) ? (temp + 1) : temp;
+			collateralAfter = roundValue;
+
+			dy = collateralBalanceBefore - collateralAfter;
+		}
+		return (dy, collateralAfter, optionBalanceAfter);
+	}
+
+    function _adjustOption(
+        address target, 
+        uint256 opt, 
+        uint256 dx, 
+        bool isBuy
+    ) internal returns (uint256 dy) {
+        IPMT t = IPMT(target);
+        require(opt < t.getOptions().length, "Invalid option selected");
+        require(dx > 0, "Deposit amount must be greater than zero");
+        
+		uint256 collateralBalanceBefore = t.getBalanceOfCollateralPool();
+		uint256 optionBalanceBefore = t.getBalanceOfOptionPool(opt);
+
+		uint256[] memory ks;
+		for (uint256 i = 0; i < t.getOptions().length; i++) {
+			ks[i] = collateralBalanceBefore * t.getBalanceOfOptionPool(i + 1);
+		}
+
+		uint256 k = collateralBalanceBefore * optionBalanceBefore;
+		uint256 collateralAfter;
+		uint256 optionBalanceAfter;
+
+		if (isBuy) {
+			collateralAfter = collateralBalanceBefore + dx;
+			optionBalanceAfter = k / collateralAfter;
+			dy = optionBalanceBefore - optionBalanceAfter;
+
+			t.setBalanceCollateralPool(collateralAfter);
+			t.setBalanceOfOptionPool(opt, optionBalanceAfter);
+
+			t.setUserTokenBalances(msg.sender, opt, dy);
+		} else {
+			require(dx <= optionBalanceBefore, "Insufficient options in pool");
+
+			optionBalanceAfter = optionBalanceBefore + dx;
+
+			uint256 temp = k / optionBalanceAfter;
+			uint256 remainder = k % optionBalanceAfter;
+			uint256 roundValue = (2 * remainder >= optionBalanceAfter) ? (temp + 1) : temp;  // 四捨五入
+			collateralAfter = roundValue;
+
+			dy = collateralBalanceBefore - collateralAfter;
+
+			t.setBalanceCollateralPool(collateralAfter);
+			t.setBalanceOfOptionPool(opt, optionBalanceAfter);
+		
+			t.setUserTokenBalances(msg.sender, opt, dx);
+			// t.burnHandler(opt, dx);
+    	}
+        return dy;
+    }
+
+	function buyOption(address target, uint256 opt, uint256 dx) public returns (uint256 acquiredOptions) {
+        return _adjustOption(target, opt, dx, true); // isBuy = true
+    }
+    function sellOption(address target, uint256 opt, uint256 dx) public returns (uint256 acquiredOptions) {
+        return _adjustOption(target, opt, dx, false); // isBuy = false
+    }
+}
---
+++ contract/contracts/PMT.sol
@@ -0,0 +1,143 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import "hardhat/console.sol";
+
+contract PMT is ERC1155, Ownable {
+    string public question;
+    address public oracleAddress;
+    address public collateralToken;
+    uint256 public collateralPoolBalance;
+    uint256 public fee;
+    uint256 public startDate;
+    uint256 public executionDate;
+    string[] public options;
+    address public allowedContract;
+    bool initLiquidityFlag = false;
+
+    // トークンのユーザーおよびプールの保有量を管理するマッピング
+    mapping(address => mapping(uint256 => uint256)) public userTokenBalances;
+    mapping(uint256 => uint256) public optionPoolBalances;  // オプショントークンのプール
+
+    constructor(
+        string memory _question,
+        address _oracleAddress,
+        address _collateralToken,
+        uint256 _fee,
+        uint256 _startDate,
+        uint256 _executionDate,
+        string[] memory _options
+    )
+      ERC1155("https://example.com/metadata/{id}.json")
+      Ownable(msg.sender)
+    {
+        question = _question;
+        oracleAddress = _oracleAddress;
+        collateralToken = _collateralToken;
+        fee = _fee;
+        startDate = _startDate;
+        executionDate = _executionDate;
+
+        // tokenID 0 = LPtoken
+        for (uint256 i = 0; i < _options.length; i++) {
+            options.push(_options[i]);
+            _mint(msg.sender, i + 1, 0, "");
+        }
+    }
+
+    modifier isStarted() {
+        require(initLiquidityFlag == true, "This market is not started.");
+        _;
+    }
+    modifier onlyAllowedContract() {
+        // require(msg.sender == allowedContract, "Not allowed contract");
+        _;
+    }
+
+    /* <<=== LIQUIDITY MANAGER ===>> */
+    function addInitLiqidity(uint256 amount) external {
+        require(!initLiquidityFlag, "Initial liquidity already added.");
+        require(amount >= 10, "Amount for each option must be greater than zero.");
+
+        collateralPoolBalance = amount;
+        for (uint256 i = 0; i < options.length; i++) {
+            optionPoolBalances[i + 1] += amount;
+            userTokenBalances[msg.sender][i + 1] += amount;
+            _mint(address(this), i + 1, amount, "");  // Mint initial liquidity for each option
+        }
+
+        require(
+            IERC20(collateralToken).transferFrom(msg.sender, address(this), collateralPoolBalance),
+            "Collateral token transfer failed"
+        );
+        initLiquidityFlag = true;
+    }
+
+    /* <<=== TRADING MANAGER ===>> */
+    function mintHandler(uint256 opt, uint256 amount) external onlyAllowedContract {
+        require(opt > 0, "Invalid option ID");
+        _mint(address(this), opt, amount, "");
+        optionPoolBalances[opt] += amount; 
+    }
+ 
+    function burnHandler(uint256 opt, uint256 amount) external onlyAllowedContract {
+        require(opt > 0, "Invalid option ID");
+        _burn(address(this), opt, amount);
+        optionPoolBalances[opt] -= amount; 
+    }
+
+    function redeemHandler(uint256 dy) external onlyAllowedContract {
+        require(opt > 0, "Invalid option ID");
+        _burn(address(this), opt, amount);
+        optionPoolBalances[opt] -= amount; 
+    }
+
+    /* <<===  SETTER FUNCTIONS  ===>> */
+    function setBalanceOfOptionPool(uint256 opt, uint256 amount) external onlyAllowedContract {
+        optionPoolBalances[opt] = amount;
+    }
+
+    function setBalanceCollateralPool(uint256 amount) external onlyAllowedContract {
+        collateralPoolBalance = amount;
+    }
+
+   function setUserTokenBalances(address user, uint256 opt, uint256 amount) external onlyAllowedContract {
+        userTokenBalances[user][opt] = amount;
+   }
+
+    /* <<===  GETTER FUNCTIONS  ===>> */
+    function balanceOfUserOption(address user, uint256 optionId) external view returns (uint256) {
+        return userTokenBalances[user][optionId];
+    }
+
+    function getBalanceOfOptionPool(uint256 optionId) external view returns (uint256) {
+        return optionPoolBalances[optionId];
+    }
+
+    function getBalanceOfCollateralPool() external view returns (uint256) {
+        return collateralPoolBalance;
+    }
+
+    function getQuestion() external view returns (string memory) {
+        return question;
+    }
+
+    function getOptions() external view returns (string[] memory) {
+        return options;
+    }
+
+    function onERC1155Received(
+        address operator,
+        address from,
+        uint256 id,
+        uint256 value,
+        bytes calldata data
+    ) external returns (bytes4) {
+        // Handle the token transfer
+        return this.onERC1155Received.selector;
+    }
+}
